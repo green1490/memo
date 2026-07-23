8bf742c16922197ea3c3ee4040a0cc39f328781b
green1490 <zambokrisztian1490@gmail.com>
2026-07-23 21:10:49 +0200

    change error handling, format and mount disk, run container

--- startup.sh
+++ startup.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
 logger "Installing podman"
-
-sudo zypper in -y podman
-
+sudo zypper in -y podman || logger "Could not install podman"
 logger "Finished installation"
 
-if [ $? -eq 0 ]; then
-    logger "Pulling image"
-    podman run --name memos -p 8080:5230 docker.io/neosmemo/memos:0.29
-    logger "Pulled image"
+logger "Formatting the persistant disk"
+sudo mkfs.ext4 /dev/disk/by-id/google-persistent-disk-1
+logger "Formated the persistant disk"
 
-    if [ $? -eq 0 ]; then
-        logger "Could not pull/run the image"
-    fi
-else
-    logger "Could not install podman"
-fi
+logger "Mounting the disk"
+sudo mkdir /mnt/data
+sudo mount /dev/disk/by-id/google-persistent-disk-1 /mnt/data || logger "Could not mount the disk"
+logger "Finished mounting the disk"
+
+mkdir /mnt/data/memos
+sudo chmod o+w /mnt/data/memos
+
+logger "Pulling image"
+podman run --name memos -p 8080:5230 -v /mnt/data/memos:/var/opt/memos:Z docker.io/neosmemo/memos:0.29|| logger "Could not pull/run the image"
+logger "Pulled image"
 
 logger "Finished startup script"
